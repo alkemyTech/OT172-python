5ac6a057843be254b2365674c5f5ba0fd7906341
leomartinez2020 <lowenhard@yahoo.com>
2022-03-18 07:16:07 -0500

    Agregar scripts de sql para extarer informacion de universidades
    - Universidad del cine
    - Universidad de Buenos Aires

---
+++ sql/universidad_del_cine.sql
@@ -0,0 +1,3 @@
+SELECT universities, careers, inscription_dates, names, sexo, birth_dates, locations
+FROM lat_sociales_cine
+WHERE universities = 'UNIVERSIDAD-DEL-CINE' and inscription_dates >= '01-09-2020' and inscription_dates <= '01-02-2021';
