0ce234499ada119121632574339be47d0a6008a4
RichardDDiaz <richardddiaz98@gmail.com>
2022-03-20 09:14:27 -0300

    creado el dag a modo de ejemplo con una prueba de juguete del flujo de tareas a realizar

---
+++ dags/etl_universidades_g.py
@@ -0,0 +1,58 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+import pathlib
+
+# Configuracion del DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': True,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5),
+    'start_date': datetime(2019, 1, 1),
+    'schedule_interval': '0 * * * *',
+    'execution_timeout': timedelta(minutes=30), #luego de 30m falla en cualquier tarea
+    'trigger_rule': 'all_success'
+}
+
+
+with DAG('etl_universidades_g', 
+        default_args=default_args,
+        template_searchpath = str(pathlib.Path().absolute()) + '/include',
+        catchup=False
+        ) as dag:
+
+        extract_sql_query_1 = BashOperator(
+            task_id='extract_sql_query_1',
+            execution_timeout=timedelta(minutes=3),
+            bash_command='echo "Ejecutando query 1"',
+            dag=dag
+        )
+
+        extract_sql_query_2 = BashOperator(
+            task_id='extract_sql_query_2',
+            execution_timeout=timedelta(minutes=3),
+            bash_command='echo "Ejecutando query 2"',
+            dag=dag
+        )
+
+        transform_pd = BashOperator(
+            task_id='transform_pd',
+            bash_command='echo "Transformando a dataframe"',
+            dag=dag
+        )
+
+        load_s3 = BashOperator(
+            task_id='load_s3',
+            bash_command='echo "Cargando a S3"',
+            dag=dag
+        )
+
+        extract_sql_query_1 >> transform_pd >> load_s3
+        extract_sql_query_2 >> transform_pd >> load_s3
+
+
+
+         
