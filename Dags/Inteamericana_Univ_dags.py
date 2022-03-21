ffbdd6d757b0e5812d4afabc7f58c3ffafaa9d6b
Juan I Solchaga <jsolchaga86@gmail.com>
2022-03-21 13:03:48 -0300

    delete task 172-48 from code

--- Dags/Inteamericana_Univ_dags.py
+++ Dags/Inteamericana_Univ_dags.py
@@ -9,18 +9,6 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 
-
-def logger():  # TAREA 172-48
-    import logging
-    logging.basicConfig(format='%(asctime)s %(logger)s %(message)s', datefmt='%Y-%m-%d',
-                       filename='logoloro.log', encoding='utf-8', level=logging.DEBUG)
-    logging.debug("")
-    logging.info("")
-    logging.warning("")
-    logging.critical("")
-    return None
-
-
 #The following configuration establishes a maximum of 5 retries, 
 # with an interval of 5 seconds between them.
 
@@ -46,16 +34,10 @@ with DAG('Interamericana_Unv',
          catchup=False,
          ) as dag:
 
-    logging_task= PythonOperator(
-        task_id= "logging",
-        python_callable= logger
-    )
-
     opr_Interamericana = PostgresOperator(
         task_id='Interam',
         postgres_conn_id='training_db',
         sql='sql-Univ-Interamericana.sql'
     )
     
-    logging_task
     opr_Interamericana
