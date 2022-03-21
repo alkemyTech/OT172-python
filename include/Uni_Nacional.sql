dd31fd0f242a031e7df3897006ffd467451c64e8
Cesarceronmanrique <ceronmanriquecesar@gmail.com>
2022-03-21 13:56:57 -0500

    Correction of SQL files to query group of universities F.

--- include/Uni_Nacional.sql
+++ include/Uni_Nacional.sql
@@ -5,12 +5,11 @@ SELECT
     ,SPLIT_PART(nacional.names,'-',1)                       AS first_name
     ,REVERSE(SPLIT_PART(REVERSE(nacional.names),'-',1))     AS last_name
     ,nacional.sexo                                          AS gender
-    ,nacional.fechas_nacimiento                             AS birth_date
+    ,nacional.fechas_nacimiento                             AS age
     ,nacional.direcciones                                   AS location
-    ,nacional.email                                        AS email
+    ,nacional.email                                         AS email
 FROM 
     rio_cuarto_interamericana AS nacional
 WHERE 
-    nacional.univiersities = 'Universidad-nacional-de-río-cuarto'
-    AND to_date(inscription_dates, 'DD/MM/YYYY') >= '01/09/2020'
-    AND to_date(inscription_dates, 'DD/MM/YYYY') <= '01/02/2021'
+    TRANSLATE(nacional.univiersities,'ÁÉÍÓÚáéíóú','AEIOUaeiou') LIKE TRANSLATE('%%Universidad-nacional-de-río-cuarto%%','ÁÉÍÓÚáéíóú','AEIOUaeiou')
+    and(DATE(inscription_dates) BETWEEN '2020-Sep-01' and  '2021-Feb-01')
