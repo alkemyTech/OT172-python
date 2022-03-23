ea672a10a2b4223a4ffbbacb31aa225628e08f7a
César Cerón Manrique <ceronmanriquecesar@gmail.com>
2022-03-23 14:48:54 -0500

    PT172-41 (#22)
    * SQL file are added to query the group of universities F
    
    * Initial DAG configuration and task structure
    
    * Syntax correction in commands
    
    * File update
    
    * Delete DAGs directory
    
    * Delete include directory
    
    * Create dag1
    
    * Create etl_university2
    
    * Update etl_university1.py
    
    * Update etl_university2.py
    
    * Correction of operators import
    
    * Configuration of the retries.
    
    * Update and rename etl_university1.py to etl_university_Nacional.py
    
    * Update etl_university_Nacional.py
    
    * Update etl_university2.py
    
    * Rename etl_university2.py to etl_university_Moron.py
    
    * Update etl_university_Moron.py

---
+++ dags/etl_university_Nacional.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+"""
+DAG configuration, without queries or processing for "Universidad Nacional De Río Cuarto"
+"""
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 5,
+    'retry_delay': timedelta(seconds=30)
+}
+with DAG(
+    'elt_university_Nacional',
+    description = 'etl for group of universities F (Universidad Nacional De Río Cuarto)',
+    schedule_interval = timedelta(hours=1),
+    default_args=default_args,
+    start_date = datetime(2022, 3, 15)
+) as dag:
+    query_task1 = DummyOperator(
+        task_id = "Query_Uni1",
+        dag = dag
+    )
+    transformation_task = DummyOperator(
+        task_id = "Transformation",
+        dag = dag
+    )
+    load_task = DummyOperator(
+        task_id = "Load",
+        dag = dag
+    )
+    query_task1 >> transformation_task >> load_task
