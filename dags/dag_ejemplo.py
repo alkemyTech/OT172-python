70478dab6d2d60e76f98f4098feff2a6110afe2c
leomartinez2020 <lowenhard@yahoo.com>
2022-03-23 06:20:32 -0500

    Agregar DAG funcional
    - En la aplicación de airflow tuvo un "recorrido feliz"

--- dags/dag_ejemplo.py
+++ dags/dag_ejemplo.py
@@ -2,47 +2,58 @@
 WIP. DAG para el procesamiento de datos sql a través de pandas
 """
 
-import datetime
-import pendulum
+from datetime import datetime, timedelta
 
-from airflow.decorators import dag, task
+from airflow import DAG
+
+from airflow.decorators import task
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 
-@dag(
-    schedule_interval="0 0 * * *",
-    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+@task
+def get_data_uba():
+    query = """
+        select
+            universidades, carreras, fechas_de_inscripcion, nombres, sexo, fechas_nacimiento, codigos_postales
+        from
+            uba_kenedy uk
+        where
+            TO_DATE(fechas_de_inscripcion, 'YY-MON-DD') between '2020-09-01' and '2021-02-01'
+            and universidades = 'universidad-de-buenos-aires'
+        limit 2
+        ;
+    """
+    postgres_hook = PostgresHook(postgres_conn_id="airflow-universities")
+    conn = postgres_hook.get_conn()
+    cur = conn.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+
+@task
+def get_data_cine():
+    query = """
+        select
+            universities, careers, inscription_dates, names, sexo, birth_dates, locations
+        from
+            lat_sociales_cine
+        where
+            TO_DATE(inscription_dates, 'DD-MM-YYYY') between '2020-09-01' and '2021-02-01'
+            and universities = 'UNIVERSIDAD-DEL-CINE'
+        limit 2
+        ;
+    """
+    postgres_hook = PostgresHook(postgres_conn_id="airflow-universities")
+    conn = postgres_hook.get_conn()
+    cur = conn.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+
+with DAG(
+    'postgres_dag',
+    #dag_id="postgres_operator_dag",
+    start_date=datetime(2022, 3, 23),
+    schedule_interval=timedelta(days=1),
+    #schedule_interval="@once",
     catchup=False,
-    dagrun_timeout=datetime.timedelta(minutes=60),
-)
-def Etl():
-    @task
-    def get_data():
-        query = """
-            select
-                universidades, carreras, fechas_de_inscripcion, nombres, sexo, fechas_nacimiento, codigos_postales
-            from
-                uba_kenedy uk
-            where
-                TO_DATE(fechas_de_inscripcion, 'YY-MON-DD') between '2020-09-01' and '2021-02-01'
-                and universidades = 'universidad-de-buenos-aires'
-            ;
-        """
-        postgres_hook = PostgresHook(postgres_conn_id="airflow-universities")
-        conn = postgres_hook.get_conn()
-        cur = conn.cursor()
-        cur.execute(query)
-        data = cur.fetchall()
-
-    @task
-    def process_data():
-        pass
-
-    @task
-    def save_data_to_s3():
-        pass
-
-    get_data() >> process_data() >> save_data_to_s3()
-
-
-dag = Etl()
+) as dag:
+    get_data_cine() >> get_data_uba()
