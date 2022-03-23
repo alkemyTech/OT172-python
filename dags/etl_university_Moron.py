700f8cdebc65c64bf53f9286f94c657d1e99e5c3
Cesar Ceron Manrique <karenyceronm@gmail.com>
2022-03-23 13:32:25 -0500

    Logs configuration

--- dags/etl_university_Moron.py
+++ dags/etl_university_Moron.py
@@ -1,6 +1,12 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python import PythonOperator
+import logging
+
+def log_function():
+    logging.basicConfig(format='%(asctime)s %(logger)s %(message)s', datefmt='%Y-%m-%d', filename='logs.log', encoding='utf-8', level=logging.DEBUG)
+    return None
 """
 DAG configuration, without queries or processing for "Universidad De Morón"
 """
@@ -19,6 +25,10 @@ with DAG(
     default_args=default_args,
     start_date = datetime(2022, 3, 15)
 ) as dag:
+    log_conf = PythonOperator(
+        task_id = "Logs_configuration",
+        python_callable = log_function
+    )
     query_task1 = DummyOperator(
         task_id = "Query_Uni2",
         dag = dag
@@ -31,4 +41,4 @@ with DAG(
         task_id = "Load",
         dag = dag
     )
-    query_task >> transformation_task >> load_task
+    log_conf >> query_task1 >> transformation_task >> load_task
