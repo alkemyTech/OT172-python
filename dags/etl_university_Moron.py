babfb536439a302592bad193ec19bb29574c9660
Cesar Ceron Manrique <ceronmanriquecesar@gmail.com>
2022-03-26 13:30:59 -0500

    Correction dataframe columns name

--- dags/etl_university_Moron.py
+++ dags/etl_university_Moron.py
@@ -1,10 +1,8 @@
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python import PythonOperator
 from decouple import config as configuracion
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 import pandas as pd
 import os
 import logging
@@ -18,28 +16,36 @@ NombreDB = configuracion("pgdb")
 URL = f"postgresql://{Usuario}:{Contra}@{Host}/{NombreDB}"
 #Funcion inicial de logging
 def log_function():
+    """
+    La funcion log_function permite aplicar la configuracion base de los logs creados en el script
+    """
     logging.basicConfig(format='%(asctime)s %(logger)s %(message)s', datefmt='%Y-%m-%d', filename='logs.log', encoding='utf-8', level=logging.DEBUG)
+    logging.info("Logs configurados con exito.")
     return None
 #Funcion de conexion a DB
 def Conexion(**kwargs):
+    """
+    La funcion conexion permite generar la conexion con la base de datos postgresql y generar un archivo .csv mediante 1 parámetro de entrada:
+        1. URL: Contiene la URL de conexion a la base de datos postgresql que debe contener (Usuario - Contraseña - Host - NombreDB).
+    """
     try:
         conexion = create_engine(kwargs['URL'], pool_size=50, echo=False)
         logging.info("Conexion a base de datos realizada con exito.")
         ruta_absoluta = os.getcwd()
-        ruta_sql = ruta_absoluta + "/airflow/include/Uni_Moron.sql" 
+        ruta_sql = ruta_absoluta + "/include/Uni_Moron.sql" 
         with open(ruta_sql, 'r', encoding="utf8") as ArchivoSQL:
             SQL_Data = ArchivoSQL.read()
             Datos = conexion.execute(SQL_Data).fetchall()
             Dataframe = pd.DataFrame(Datos)
         logging.info("Consulta realizada con exito.")
-        Dataframe.to_csv(ruta_absoluta + "/airflow/files/Uni_Moron.csv", index=False, encoding="utf-8")
+        Dataframe.set_axis(['university', 'career', 'inscription_date', 'first_name', 'last_name', 'gender', 'age', 'location', 'email'], axis = 'columns', inplace = True)
+        Dataframe.to_csv(ruta_absoluta + "/files/Uni_Moron.csv", index=False, encoding="utf-8")
         logging.info("Dataframe Universidad de Moron exportado a csv con exito.")
         return None
     except Exception as e:
         logging.error(e)
-#Funcion exportar Dataframe a csv
 """
-DAG configuration, without queries or processing for "Universidad De Morón"
+Configuracion del DAG con 2 operadores de Python para la Universidad Nacional de Rio Cuarto."
 """
 default_args = {
     'owner': 'airflow',
