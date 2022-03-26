d5b75beb465b2b97b2b3a5e05e8f19f768c3b17e
leomartinez2020 <lowenhard@yahoo.com>
2022-03-26 10:54:05 -0500

    WIP: prcesar datos 2 universidades

---
+++ dags/procesar_datos_uni_villa_maria.py
@@ -0,0 +1,10 @@
+"""
+Procesar los datos obtenidos de la base de datos dentro del DAG
+
+Configurar el Python Operator para que ejecute las dos funciones que procese los datos para las siguientes universidades:
+Universidad De Flores
+Universidad Nacional De Villa María
+
+Documentación
+https://airflow.apache.org/docs/apache-airflow/stable/_api/airflow/operators/python/index.html#module-airflow.operators.python
+"""
