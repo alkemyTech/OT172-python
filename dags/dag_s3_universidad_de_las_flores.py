d62ca987a46489ca8d8dea27779b4372b2329d3b
leomartinez2020 <lowenhard@yahoo.com>
2022-03-26 10:48:05 -0500

    WIP: python flores to s3

---
+++ dags/dag_s3_universidad_de_las_flores.py
@@ -0,0 +1,8 @@
+"""
+Poder subir el txt creado por el operador de Python al S3
+
+Tomar el .txt del repositorio base
+Buscar un operador creado por la comunidad que se adecue a los datos.
+Configurar el S3 Operator para la Universidad De Flores
+Subir el archivo a S3
+"""
