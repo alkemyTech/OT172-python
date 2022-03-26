987c0da26fd8a0840c85688edb66c490adb6ccfb
leomartinez2020 <lowenhard@yahoo.com>
2022-03-26 10:51:46 -0500

    WIP: funcion pandas 2 universidades

---
+++ dags/funcion_pandas_uni_villa_maria.py
@@ -0,0 +1,21 @@
+"""
+Una funcion que devuelva un txt para cada una de las siguientes universidades con los datos normalizados:
+Universidad De Flores
+Universidad Nacional De Villa María
+
+Datos Finales:
+university: str minúsculas, sin espacios extras, ni guiones
+career: str minúsculas, sin espacios extras, ni guiones
+inscription_date: str %Y-%m-%d format
+first_name: str minúscula y sin espacios, ni guiones
+last_name: str minúscula y sin espacios, ni guiones
+gender: str choice(male, female)
+age: int
+postal_code: str
+location: str minúscula sin espacios extras, ni guiones
+email: str minúsculas, sin espacios extras, ni guiones
+
+Aclaraciones:
+Para calcular codigo postal o locación se va a utilizar el .csv que se encuentra en el repo.
+La edad se debe calcular en todos los casos
+"""
