968057b1628a28eacc8699dbf5b3bf7fb8b31079
César Cerón Manrique <ceronmanriquecesar@gmail.com>
2022-03-28 06:47:15 -0500

    Pt172-65 (#26)
    * SQL file are added to query the group of universities F
    
    * Initial DAG configuration and task structure
    
    * Syntax correction in commands
    
    * File update
    
    * Delete DAGs directory
    
    * Delete include directory
    
    * Create dag1
    
    * Create etl_university2
    
    * Update etl_university1.py
    
    * Update etl_university2.py
    
    * Correction of operators import
    
    * Configuration of the retries.
    
    * Update and rename etl_university1.py to etl_university_Nacional.py
    
    * Update etl_university_Nacional.py
    
    * Update etl_university2.py
    
    * Rename etl_university2.py to etl_university_Moron.py
    
    * Logs configuration
    
    * Files are added to perform SQL query from dags with PythonOperator and generate .csv in the files directory.
    
    * Dags update.
    
    * Correction dataframe columns name
    
    Co-authored-by: Cesar Ceron Manrique <karenyceronm@gmail.com>
    Co-authored-by: Fernando Palacios <fernandogabrielpalacios@gmail.com>

--- dags/etl_university_Nacional.py
+++ dags/etl_university_Nacional.py
@@ -1,15 +1,51 @@
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python import PythonOperator
+from decouple import config as configuracion
+from sqlalchemy import create_engine
+import pandas as pd
+import os 
 import logging
 
+#Configuracion de parametros de .env
+Usuario = configuracion("pguser")
+Contra = configuracion("pgpass")
+Host = configuracion("pghost")
+Puerto = configuracion("pgport")
+NombreDB = configuracion("pgdb")
+URL = f"postgresql://{Usuario}:{Contra}@{Host}/{NombreDB}"
+#Funcion inicial de logging
 def log_function():
+    """
+    La funcion log_function permite aplicar la configuracion base de los logs creados en el script
+    """
     logging.basicConfig(format='%(asctime)s %(logger)s %(message)s', datefmt='%Y-%m-%d', filename='logs.log', encoding='utf-8', level=logging.DEBUG)
+    logging.info("Logs configurados con exito.")
     return None
-
+#Funcion de conexion a DB
+def Conexion(**kwargs):
+    """
+    La funcion conexion permite generar la conexion con la base de datos postgresql y generar un archivo .csv mediante 1 parámetro de entrada:
+        1. URL: Contiene la URL de conexion a la base de datos postgresql que debe contener (Usuario - Contraseña - Host - NombreDB).
+    """
+    try:
+        conexion = create_engine(kwargs['URL'], pool_size=50, echo=False)
+        logging.info("Conexion a base de datos realizada con exito.")
+        ruta_absoluta = os.getcwd()
+        ruta_sql = ruta_absoluta + "/include/Uni_Nacional.sql" 
+        with open(ruta_sql, 'r', encoding="utf8") as ArchivoSQL:
+            SQL_Data = ArchivoSQL.read()
+            Datos = conexion.execute(SQL_Data).fetchall()
+            Dataframe = pd.DataFrame(Datos)
+        logging.info("Consulta realizada con exito.")
+        Dataframe.set_axis(['university', 'career', 'inscription_date', 'first_name', 'last_name', 'gender', 'age', 'location', 'email'], axis = 'columns', inplace = True)
+        Dataframe.to_csv(ruta_absoluta + "/files/Uni_Nacional.csv", index=False, encoding="utf-8")
+        logging.info("Dataframe Universidad Nacional de Rio Cuarto exportado a csv con exito.")
+        return None
+    except Exception as e:
+        logging.error(e)
 """
-DAG configuration, without queries or processing for "Universidad Nacional De Río Cuarto"
+Configuracion del DAG con 2 operadores de Python para la Universidad Nacional de Rio Cuarto."
 """
 default_args = {
     'owner': 'airflow',
@@ -21,7 +57,7 @@ default_args = {
 }
 with DAG(
     'elt_university_Nacional',
-    description = 'etl for group of universities F (Universidad Nacional De Río Cuarto)',
+    description = 'etl for group of universities F (Universidad Nacional de Río Cuarto)',
     schedule_interval = timedelta(hours=1),
     default_args=default_args,
     start_date = datetime(2022, 3, 15)
@@ -30,17 +66,12 @@ with DAG(
         task_id = "Logs_configuration",
         python_callable = log_function
     )
+    query_task = PythonOperator(
+        task_id = "Query_Uni_Moron",
+        python_callable = Conexion,
+        op_kwargs={
+            'URL':URL
+        }
+    )
+    log_conf >> query_task
 
-    query_task1 = DummyOperator(
-        task_id = "Query_Uni1",
-        dag = dag
-    )
-    transformation_task = DummyOperator(
-        task_id = "Transformation",
-        dag = dag
-    )
-    load_task = DummyOperator(
-        task_id = "Load",
-        dag = dag
-    )
-    log_conf >> query_task1 >> transformation_task >> load_task
