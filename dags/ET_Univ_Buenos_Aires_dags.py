4d66bda67094bbcdb3f5b8ff022a05ee8494b7eb
leomartinez2020 <lowenhard@yahoo.com>
2022-03-27 20:57:00 -0500

    Agregar DAGS de python operator PT172-67

---
+++ dags/ET_Univ_Buenos_Aires_dags.py
@@ -0,0 +1,49 @@
+"""
+PT172-67
+Configurar un Python Operators, para que extraiga información de la base de datos
+utilizando el .sql disponible en el repositorio base de las siguientes universidades:
+Universidad De Buenos Aires
+Dejar la información en un archivo .csv dentro de la carpeta files.
+"""
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+import pandas as pd
+
+import sqlparse
+
+def get_data_uba():
+    sql_src = '/home/lowenhard/airflow/include/universidad_de_buenos_aires.sql'
+    with open(sql_src, 'r') as sqlfile:
+        query = sqlfile.read()
+    query = sqlparse.format(query, strip_comments=True).strip()
+    postgres_hook = PostgresHook(postgres_conn_id="airflow-universities")
+    conn = postgres_hook.get_conn()
+    cur = conn.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+    df = pd.DataFrame(data=data)
+    header = ['universidades', 'carreras', 'fechas_de_inscripcion', 'nombres', 'sexo', 'fechas_nacimiento', 'codigos_postales']
+    df.to_csv('/home/lowenhard/airflow/files/ET_Univ_Buenos_Aires.csv', header=header, index=False)
+    return data
+
+with DAG(
+    'python_operator_uba',
+    start_date=datetime(2020, 3, 26),
+    template_searchpath='/home/lowenhard/airflow/include',
+    catchup=False,
+    tags=['operator']
+) as dag:
+    t0 = DummyOperator(task_id='start')
+    t1 = PythonOperator(
+        task_id='create_csv',
+        python_callable=get_data_uba,
+        dag=dag,
+    )
+    t2 = DummyOperator(task_id='end')
+
+t0 >> t1 >> t2
---
+++ dags/ET_Univ_del_Cine_dags.py
@@ -0,0 +1,49 @@
+"""
+PT172-67
+Configurar un Python Operators, para que extraiga información de la base de datos
+utilizando el .sql disponible en el repositorio base de las siguientes universidades:
+Universidad Del Cine
+Dejar la información en un archivo .csv dentro de la carpeta files.
+"""
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+import pandas as pd
+
+import sqlparse
+
+def get_data_cine():
+    sql_src = '/home/lowenhard/airflow/include/universidad_del_cine.sql'
+    with open(sql_src, 'r') as sqlfile:
+        query = sqlfile.read()
+    query = sqlparse.format(query, strip_comments=True).strip()
+    postgres_hook = PostgresHook(postgres_conn_id="airflow-universities")
+    conn = postgres_hook.get_conn()
+    cur = conn.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+    df = pd.DataFrame(data=data)
+    header = ['universities', 'careers', 'inscription_dates', 'names', 'sexo', 'birth_dates', 'locations']
+    df.to_csv('/home/lowenhard/airflow/files/ET_Univ_del_Cine.csv', header=header, index=False)
+    return data
+
+with DAG(
+    'python_operator_cine',
+    start_date=datetime(2020, 3, 26),
+    template_searchpath='/home/lowenhard/airflow/include',
+    catchup=False,
+    tags=['operator']
+) as dag:
+    t0 = DummyOperator(task_id='start')
+    t1 = PythonOperator(
+        task_id='create_csv',
+        python_callable=get_data_cine,
+        dag=dag,
+    )
+    t2 = DummyOperator(task_id='end')
+
+t0 >> t1 >> t2
