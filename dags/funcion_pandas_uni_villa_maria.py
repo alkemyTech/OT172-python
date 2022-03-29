03f1eb18f37904d923cf9fb693c85d48a5f132ba
leomartinez2020 <lowenhard@yahoo.com>
2022-03-29 08:41:23 -0500

    WIP branch PT172-76

--- dags/funcion_pandas_uni_villa_maria.py
+++ dags/funcion_pandas_uni_villa_maria.py
@@ -18,4 +18,10 @@ email: str minúsculas, sin espacios extras, ni guiones
 Aclaraciones:
 Para calcular codigo postal o locación se va a utilizar el .csv que se encuentra en el repo.
 La edad se debe calcular en todos los casos
+CSV:
+universidad,carrera,fecha_de_inscripcion,nombre_de_usuario,sexo,fecha_nacimiento,direccion,email
+UNIVERSIDAD_NACIONAL_DE_VILLA_MARÍA,LICENCIATURA_EN_CIENCIA_POLÍTICA_,15-Sep-20,WELCHMIGUEL,M,05-Oct-15,"594_ALVARADO_STREET_SUITE_945
+CHANSIDE,_IL_06393",RICHARD21@HOTMAIL.COM
+UNIVERSIDAD_NACIONAL_DE_VILLA_MARÍA,LICENCIATURA_EN_PLANIFICACIÓN_Y_DISEÑO_DEL_PAISAJE,20-Sep-20,RACHELFLORES,F,18-Sep-67,"9556_RYAN_FORD
+
 """
--- dags/funcion_pandas_uniflores.py
+++ dags/funcion_pandas_uniflores.py
@@ -1,4 +1,6 @@
 """
+Pandas Uni A Leonardo
+
 Una funcion que devuelva un txt para cada una de las siguientes universidades con los datos normalizados:
 Universidad De Flores
 Universidad Nacional De Villa María
@@ -18,4 +20,22 @@ email: str minúsculas, sin espacios extras, ni guiones
 Aclaraciones:
 Para calcular codigo postal o locación se va a utilizar el .csv que se encuentra en el repo.
 La edad se debe calcular en todos los casos
+
+SELECT
+    universidad,
+    carrera,
+    fecha_de_inscripcion,
+    nombre_de_usuario,
+    sexo,
+    fecha_nacimiento,
+    codigo_postal,
+    direccion,
+    correo_electronico
+FROM
+    flores_comahue
+WHERE
+    (fecha_de_inscripcion BETWEEN '2020-09-01' AND '2021-02-01')
+AND
+    universidad = 'UNIVERSIDAD DE FLORES'
+
 """
