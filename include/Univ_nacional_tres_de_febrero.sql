1d770329875b83c8cdc147cc2446b81622f627f0
Juan I Solchaga <jsolchaga86@gmail.com>
2022-03-29 06:57:40 -0300

    correcciones en la funcion de transformacion

--- include/Univ_nacional_tres_de_febrero.sql
+++ include/Univ_nacional_tres_de_febrero.sql
@@ -1,4 +1,4 @@
---Query to obtain raw data on people enrolled in Universodad Interamericana
+--Query to obtain raw data on people enrolled in Universodad tres de febrero
 SELECT
 universidad as university,
 careers as career, 
