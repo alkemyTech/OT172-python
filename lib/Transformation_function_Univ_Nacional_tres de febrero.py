d9d8a5fe61a64bdd9d19c3e9580af3068336d675
Juan I Solchaga <jsolchaga86@gmail.com>
2022-04-01 13:10:36 -0300

    conflictos solucionados

--- lib/Transformation_function_Univ_Nacional_tres de febrero.py
+++ lib/Transformation_function_Univ_Nacional_tres de febrero.py
@@ -20,57 +20,43 @@ def normalize_characters(column):
     return column
 
 
-def transformation(df, univ):
+
+def transformation(df):
     path=(pathlib.Path(__file__).parent.absolute()).parent
     logging.info(f'normalizing data')
     df['university'] = normalize_characters(df['university'])
     df['career'] = normalize_characters(df['career'])
-    df['name'] = normalize_characters(df['name'])
-    df['gender'] = normalize_characters(df['gender'])
     df['gender'] = df['gender'].apply(
-        lambda x: 'male' if x[0] == 'm' else 'female')
+        lambda x: 'male' if x == 'm' else 'female')
 
     old_date = pd.to_datetime(df['inscription_date'])
     df['inscription_date'] = pd.to_datetime(old_date, '%Y/%m/%d')
 
-    df['first_name'] = df['name'].apply(lambda x: str(x).split(' ')[0])
-    df['last_name'] = df['name'].apply(lambda x: str(x).split(' ')[1])
+    df['first_name'] = df['name'].apply(lambda x: str(x).split('_')[0])
+    df['last_name'] = df['name'].apply(lambda x: str(x).split('_')[1])
 
-
-
-    curr= datetime.now()
+    curr = datetime.now()
     df['age'] = df['nacimiento'].apply(lambda x: (
-        100+(int(str(curr.year)[2:4]) - int(x[7:9])) if len(x)== 9 else (curr.year - datetime.strptime(str(x), '%Y/%m/%d').year)))
+        100+(int(str(curr.year)[2:4]) - int(x[7:9]))))
 
-    if 'postal_code' in df.columns:
-        input= 'postal_code'
-        output= 'location'
-        key= 'codigo_postal'
-        value= 'localidad'
-    else:
-        input= 'location'
-        output= 'postal_code'
-        key= 'localidad'
-        value= 'codigo_postal'
+    df_postal_codes = (pd.read_csv(f'{path}/dataset/codigos_postales.csv'))
+    df_postal_codes['localidad'] = df_postal_codes['localidad'].apply(
+        lambda x: x.lower())
+    dict_postal_codes = dict(
+        zip(df_postal_codes.codigo_postal, df_postal_codes.localidad))
+    df['location'] = df['postal_code'].apply(lambda x: dict_postal_codes[int(x)])
 
-        df_postal_codes = (pd.read_csv(f'{path}/dataset/codigos_postales.csv'))
-        df_postal_codes[key] = df_postal_codes[key].apply(
-            lambda x: x.lower())
-        dict_postal_codes = dict(
-            zip(df_postal_codes[key], df_postal_codes[value]))
-        df[output] = df[input].apply(lambda x: dict_postal_codes[(x)])
-
-        df = df[['university', 'career', 'inscription_date', 'first_name',
-                'last_name', 'gender', 'age', 'postal_code', 'location', 'email']]
-        df.to_csv(f'{path}/files/ETL_{univ}.txt', sep='\t')
-
+    df = df[['university', 'career', 'inscription_date', 'first_name',
+             'last_name', 'gender', 'age', 'postal_code', 'location', 'email']]
+    df.to_csv(f'{path}/files/ET_Univ_nacional_tres_de_febrero.txt', sep='\t')
+    return(df)
 
 
 def main():
     path_input = (pathlib.Path(__file__).parent.absolute()).parent
     path= f'{path_input}/files/Extraction_Univ_nacional_tres_de_febrero.csv'
     df= pd.read_csv(path)
-    transformation(df, 'febr')
+    transformation(df)
 
 if __name__=='__main__':
     main()
--- lib/Transformation_function_Unive_tecnologica_nacional.py
+++ lib/Transformation_function_Unive_tecnologica_nacional.py
@@ -40,15 +40,13 @@ def normalize_characters(column):
 # previously defined, resulting in the corresponding localities column).
 # line 17: only the required columns were selected
 
-def transformation(df, univ):
-    path=(pathlib.Path(__file__).parent.absolute()).parent
+def transformation(df):
     logging.info(f'normalizing data')
     df['university'] = normalize_characters(df['university'])
     df['career'] = normalize_characters(df['career'])
-    df['name'] = normalize_characters(df['name'])
-    df['gender'] = normalize_characters(df['gender'])
+    df['location'] = normalize_characters(df['location'])
     df['gender'] = df['gender'].apply(
-        lambda x: 'male' if x[0] == 'm' else 'female')
+        lambda x: 'male' if x == 'm' else 'female')
 
     old_date = pd.to_datetime(df['inscription_date'])
     df['inscription_date'] = pd.to_datetime(old_date, '%Y/%m/%d')
@@ -56,33 +54,20 @@ def transformation(df, univ):
     df['first_name'] = df['name'].apply(lambda x: str(x).split(' ')[0])
     df['last_name'] = df['name'].apply(lambda x: str(x).split(' ')[1])
 
-
-
-    curr= datetime.now()
+    curr = datetime.now()
     df['age'] = df['nacimiento'].apply(lambda x: (
-        100+(int(str(curr.year)[2:4]) - int(x[7:9])) if len(x)== 9 else (curr.year - datetime.strptime(str(x), '%Y/%m/%d').year)))
+        curr.year - datetime.strptime(str(x), '%Y/%m/%d').year))
 
-    if 'postal_code' in df.columns:
-        input= 'postal_code'
-        output= 'location'
-        key= 'codigo_postal'
-        value= 'localidad'
-    else:
-        input= 'location'
-        output= 'postal_code'
-        key= 'localidad'
-        value= 'codigo_postal'
+    df_postal_codes = (pd.read_csv(f'{path}/dataset/codigos_postales.csv'))
+    df_postal_codes['localidad'] = df_postal_codes['localidad'].apply(
+        lambda x: x.lower())
+    dict_postal_codes = dict(
+        zip(df_postal_codes.localidad, df_postal_codes.codigo_postal))
+    df['postal_code'] = df['location'].apply(lambda x: dict_postal_codes[x])
 
-        df_postal_codes = (pd.read_csv(f'{path}/dataset/codigos_postales.csv'))
-        df_postal_codes[key] = df_postal_codes[key].apply(
-            lambda x: x.lower())
-        dict_postal_codes = dict(
-            zip(df_postal_codes[key], df_postal_codes[value]))
-        df[output] = df[input].apply(lambda x: dict_postal_codes[(x)])
-
-        df = df[['university', 'career', 'inscription_date', 'first_name',
-                'last_name', 'gender', 'age', 'postal_code', 'location', 'email']]
-        df.to_csv(f'{path}/files/ETL_{univ}.txt', sep='\t')
+    df = df[['university', 'career', 'inscription_date', 'first_name',
+             'last_name', 'gender', 'age', 'postal_code', 'location', 'email']]
+    df.to_csv(f'{path}/files/ET_Univ_tecnologica_nacional.txt', header=None, index=None, sep='\t', mode='a')
     return(df)
 
 
@@ -90,7 +75,7 @@ def main():
     path_input = (pathlib.Path(__file__).parent.absolute()).parent
     path= f'{path_input}/files/Extraction_Univ_tecnologica_nacional.csv'
     df= pd.read_csv(path)
-    transformation(df, 'tecno')
+    transformation(df)
 
 if __name__=='__main__':
     main()
