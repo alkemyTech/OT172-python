6ff44a4f17b76adfef42b0ef15b417e53ceaa0a2
leomartinez2020 <lowenhard@yahoo.com>
2022-04-05 05:17:05 -0500

    WIP: Dag villa maria

---
+++ dags/dag_s3_universidad_villa_maria.py
@@ -0,0 +1,51 @@
+"""
+Poder subir el txt creado por el operador de Python al S3
+
+Tomar el .txt del repositorio base
+Buscar un operador creado por la comunidad que se adecue a los datos.
+Configurar el S3 Operator para la Universidad De Villa Maria
+Subir el archivo a S3
+"""
+import logging
+from pathlib import Path
+import os
+
+from datetime import datetime
+from airflow.models import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.hooks.S3_hook import S3Hook
+
+folder = Path(__file__).resolve().parent.parent
+log = logging.getLogger(__name__)
+
+def upload_to_s3(filename, key, bucket_name):
+    hook = S3Hook('s3-connection')
+    hook.load_file(filename=filename, key=key, bucket_name=bucket_name)
+    has_key = hook.check_for_key(key, bucket_name=bucket_name)
+    print(filename, key, bucket_name)
+    print("Created Connection")
+    print(hook.get_session())
+    print(str(has_key))
+    log.info(f'File was copied to s3: {str(has_key)}')
+
+with DAG(
+    dag_id='s3_uni_villa_maria',
+    schedule_interval='@daily',
+    start_date=datetime(2022, 3, 1),
+    catchup=False
+) as dag:
+    task_upload_to_s3 = PythonOperator(
+        task_id='upload_to_s3',
+        python_callable=upload_to_s3,
+        op_kwargs={
+            'filename': os.path.join(folder, 'files/ET_Univ_Villa_Maria.txt'),
+            'key': 'ET_Univ_Villa_Maria.txt',
+            'bucket_name': 'cohorte-marzo-77238c6a'
+        }
+    )
+
+    t0 = DummyOperator(task_id='start')
+    t1 = task_upload_to_s3
+
+t0 >> t1
