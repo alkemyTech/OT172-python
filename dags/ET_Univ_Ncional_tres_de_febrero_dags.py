0db8f267a0dfb4e68cf3493720a0c7e03ee49440
Juan I Solchaga <jsolchaga86@gmail.com>
2022-04-06 16:34:12 -0300

    correccion

--- dags/ET_Univ_Ncional_tres_de_febrero_dags.py
+++ dags/ET_Univ_Ncional_tres_de_febrero_dags.py
@@ -1,33 +1,39 @@
-import os
+from asyncio import Task
 from airflow import DAG
-import datetime
-from datetime import datetime, timedelta
-from decouple import config
-import pathlib
-import logging
-from lib.connection_func import load_s3
-from lib.logg import logger
-from lib.transform_all_data import *
+from airflow.hooks.S3_hook import S3Hook
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python import PythonOperator
+from datetime import timedelta
+from datetime import datetime
 from airflow.models import Connection
-from airflow.hooks.S3_hook import S3Hook
-
-
-# Credentials,  path & table id:
-
-TABLE_ID= 'Univ_nacional_tres_de_febrero'
-PG_ID= config('PG_ID', default='')
-
+from airflow import settings
+from sympy import Id
+import pandas as pd
+import logging
+import os
+import sys
+from decouple import config
 # To define the directory, the pathlib.Path(__file__) function of the payhlib module was used.
 #  This function detects the path of the running .py file. Since that file is in /dags, it is
 #  necessary to move up one level. This is achieved with the .parent method.
-path = (pathlib.Path(__file__).parent.absolute()).parent
+import pathlib
 
+path_p = (pathlib.Path(__file__).parent.absolute()).parent
+  
+sys.path.append(f'/{path_p}/lib')
+from functions_Project_1 import *
 
-# Functions
-# Function to define logs, using the logging library: https:/docs.python.org/3/howto/logging.html
+# Credentials,  path & table id:
 
+TABLE_ID= 'Univ_tecnologica_nacional'
+PG_ID= config('PG_ID', default='')
+S3_ID=config('S3_ID', default='')
+BUCKET_NAAME=config('V', default='')
+PUBLIC_KEY=config('PUBLIC_KEY', default='')
+
+# root
+# Function to define logs, using the logging library: https://docs.python.org/3/howto/logging.html
 
 # Retries configuration
 default_args = {
@@ -39,35 +45,33 @@ default_args = {
     'retry_delay': timedelta(seconds=30)
 }
 
-# Dag definition for the E process
-with DAG('Extraction_Univ_LaPamPa',
-         description='for the execution of an sql query to the trainingn\
-          database, to obtain information about Universidad tres de febrero',
-         start_date=datetime(2020, 3, 24),
+# Dag definition for the ETL process
+with DAG('ETL_Univ_nacional_tres_de_febrero',
+         start_date = datetime(2022, 3, 15),
          max_active_runs=3,
          schedule_interval='@hourly',
          default_args=default_args,
-         template_searchpath=f'{path}/include',
+         template_searchpath=f'{path_p}/airflow/include',
          catchup=False,
          ) as dag:
 
+
 # PythonOperator for extraaction function, commented above
-    extraction_task = PythonOperator(
+    ET_task = PythonOperator(
         task_id="Extraction",
         python_callable=extraction_transform_data,
         op_kwargs={'database_id': 'training_db',
                    'table_id': TABLE_ID}
 
     )
-
 # PythonOperator for logger function, commented above
     logging_task = PythonOperator(
         task_id="logguers",
         python_callable=logger,
-        op_args= {f'{path}/logs/{TABLE_ID}'}
+        op_args= {f'{path_p}/logs/{TABLE_ID}'}
     )
 
 
 
-    logging_task 
-    extraction_task
+    logging_task
+    ET_task
--- dags/ET_Univ_Tecnologica_Nacional_dags.py
+++ dags/ET_Univ_Tecnologica_Nacional_dags.py
@@ -1,32 +1,39 @@
-import os
+from asyncio import Task
 from airflow import DAG
-import datetime
-from datetime import datetime, timedelta
-from decouple import config
-import pathlib
-import logging
-from lib.connection_func import load_s3
-from lib.logg import logger
-from lib.transform_all_data import *
+from airflow.hooks.S3_hook import S3Hook
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python import PythonOperator
+from datetime import timedelta
+from datetime import datetime
 from airflow.models import Connection
-from airflow.hooks.S3_hook import S3Hook
+from airflow import settings
+from sympy import Id
+import pandas as pd
+import logging
+import os
+import sys
+from decouple import config
+# To define the directory, the pathlib.Path(__file__) function of the payhlib module was used.
+#  This function detects the path of the running .py file. Since that file is in /dags, it is
+#  necessary to move up one level. This is achieved with the .parent method.
+import pathlib
 
+path_p = (pathlib.Path(__file__).parent.absolute()).parent
+  
+sys.path.append(f'/{path_p}/lib')
+from functions_Project_1 import *
 
 # Credentials,  path & table id:
 
 TABLE_ID= 'Univ_tecnologica_nacional'
 PG_ID= config('PG_ID', default='')
+S3_ID=config('S3_ID', default='')
+BUCKET_NAAME=config('V', default='')
+PUBLIC_KEY=config('PUBLIC_KEY', default='')
 
-# To define the directory, the pathlib.Path(__file__) function of the payhlib module was used.
-#  This function detects the path of the running .py file. Since that file is in /dags, it is
-#  necessary to move up one level. This is achieved with the .parent method.
-path = (pathlib.Path(__file__).parent.absolute()).parent
-
-# Functions
-# Function to define logs, using the logging library: https:/docs.python.org/3/howto/logging.html
-
+# root
+# Function to define logs, using the logging library: https://docs.python.org/3/howto/logging.html
 
 # Retries configuration
 default_args = {
@@ -38,51 +45,38 @@ default_args = {
     'retry_delay': timedelta(seconds=30)
 }
 
-# Dag definition for the E process
-with DAG('Extraction_Univ_LaPamPa',
-         description='for the execution of an sql query to the trainingn\
-          database, to obtain information about Universidad tres de febrero',
-
-         start_date=datetime(2020, 3, 24),
+# Dag definition for the ETL process
+with DAG('ETL_Univ_nacional_tres_de_febrero',
+         start_date = datetime(2022, 3, 15),
          max_active_runs=3,
          schedule_interval='@hourly',
          default_args=default_args,
-         template_searchpath=f'{path}/include',
+         template_searchpath=f'{path_p}/airflow/include',
          catchup=False,
          ) as dag:
 
+
 # PythonOperator for extraaction function, commented above
-    extraction_task = PythonOperator(
+    ET_task = PythonOperator(
         task_id="Extraction",
         python_callable=extraction_transform_data,
         op_kwargs={'database_id': 'training_db',
                    'table_id': TABLE_ID}
+
     )
-
-# PythonOperator for ETL function, commented above
-    load_task = PythonOperator(
-        task_id="Load",
-        python_callable=load_s3_function
-    )
-
-    connect_to_s3 = PythonOperator(
-            task_id="s3_connection",
-            python_callable=get_connection,
-            op_kwargs={'buket_name': BUCKET_NAME,
-            'conntype': S3_ID,
-            'secret_key':S3_SECRET_KEY,
-            'public_key':S3_PUBLIC_KEY,
-            'id':'s3_con'} 
-        )
-
-
 # PythonOperator for logger function, commented above
     logging_task = PythonOperator(
         task_id="logguers",
         python_callable=logger,
-        op_args= {f'{path}/logs/{TABLE_ID}'}
+        op_args= {f'{path_p}/logs/{TABLE_ID}'}
     )
 
-    logging_task 
-    extraction_task
+  # PythonOperator for ETL function, commented above
+    load_task = PythonOperator(
+        task_id="Load",
+        python_callable=load_s3,
+        op_kwargs={'id_conn': S3_ID, 'bucket_name': BUCKET_NAAME, 'key': PUBLIC_KEY}
+    )
 
+    logging_task
+    ET_task >>   load_task
