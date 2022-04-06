0b784be9e687b0fa0f5bb3bea5ad9de85ce77db1
Juan I Solchaga <jsolchaga86@gmail.com>
2022-04-06 00:59:15 -0300

    gregdo de credenciales

--- dags/ETL_Univ_Ncional_tres_de_febrero_dags.py
+++ dags/ETL_Univ_Ncional_tres_de_febrero_dags.py
@@ -8,6 +8,7 @@ import logging
 from lib.connection_func import load_s3
 from lib.logg import logger
 from lib.transform_all_data import *
+from lib.connection_func import *
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python import PythonOperator
 from airflow.models import Connection
@@ -18,15 +19,18 @@ from airflow.hooks.S3_hook import S3Hook
 
 TABLE_ID= 'Univ_nacional_tres_de_febrero'
 PG_ID= config('PG_ID', default='')
+S3_ID=config('S3_ID', default='')
+BUCKET_NAAME=config('V', default='')
+PUBLIC_KEY=config('PUBLIC_KEY', default='')
 
 # To define the directory, the pathlib.Path(__file__) function of the payhlib module was used.
 #  This function detects the path of the running .py file. Since that file is in /dags, it is
 #  necessary to move up one level. This is achieved with the .parent method.
 path = (pathlib.Path(__file__).parent.absolute()).parent
 
+
 # Functions
 # Function to define logs, using the logging library: https:/docs.python.org/3/howto/logging.html
-
 
 
 # Retries configuration
@@ -43,7 +47,6 @@ default_args = {
 with DAG('Extraction_Univ_LaPamPa',
          description='for the execution of an sql query to the trainingn\
           database, to obtain information about Universidad tres de febrero',
-
          start_date=datetime(2020, 3, 24),
          max_active_runs=3,
          schedule_interval='@hourly',
@@ -61,10 +64,13 @@ with DAG('Extraction_Univ_LaPamPa',
     )
 
 # PythonOperator for ETL function, commented above
-
     load_task = PythonOperator(
         task_id="Load",
-        python_callable=load_s3_function
+        python_callable=load_s3_function,
+        op_args={'path': path,
+         'id_conn': S3_ID,
+         'bucket_name': BUCKET_NAAME,
+          'key': PUBLIC_KEY}
     )
 
 # PythonOperator for logger function, commented above
@@ -78,4 +84,3 @@ with DAG('Extraction_Univ_LaPamPa',
 
     logging_task 
     extraction_task >> load_task
-
