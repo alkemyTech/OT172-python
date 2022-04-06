55f4b8f0b99cd5b3a950b1c33fc2ab04db9b9f64
Juanis86 <65877667+Juanis86@users.noreply.github.com>
2022-04-06 15:50:21 -0300

    Pt172 64 (#68)
    * issues PT172-64
    
    * docstrings logs and code optimization
    
    * agregado el codigo necesario para importar funciones desde otra carpta/ agregado de un archivo __init___.pyen la carpeta dags y otro en a carpeta lib, para poder hacer esta importacion
    
    * agregado el codigo necesario para importar funciones desde otra carpta/ agregado de un archivo __init___.pyen la carpeta dags y otro en a carpeta lib, para poder hacer esta importacion
    
    * correccion
    
    * correccion

--- dags/ET_Univ_Nacional_tres_de_febrero_dags.py
+++ dags/ET_Univ_Nacional_tres_de_febrero_dags.py
@@ -1,11 +1,15 @@
-import os
+from asyncio import Task
 from airflow import DAG
-import datetime
-from datetime import datetime, timedelta
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python import PythonOperator
-from decouple import config
-import pathlib
+from datetime import datetime, timedelta
+import datetime
+from datetime import datetime
 import logging
+from decouple import config
+import os
+import pathlib
 import sys
 # To define the directory, the pathlib.Path(__file__) function of the payhlib module was used.
 #  This function detects the path of the running .py file. Since that file is in /dags, it is
@@ -20,9 +24,11 @@ from functions_Project_1 import *
 TABLE_ID= 'Univ_nacional_tres_de_febrero'
 PG_ID= config('PG_ID', default='')
 S3_ID=config('S3_ID', default='')
-S3_ID=config('S3_ID', default='')
-BUCKET_NAAME=config('BUCKET_NAME', default='')
+BUCKET_NAAME=config('V', default='')
 PUBLIC_KEY=config('PUBLIC_KEY', default='')
+
+# root
+# Function to define logs, using the logging library: https://docs.python.org/3/howto/logging.html
 
 # Retries configuration
 default_args = {
@@ -34,33 +40,31 @@ default_args = {
     'retry_delay': timedelta(seconds=30)
 }
 
-# Dag definition for the E process
-with DAG('Extraction_Univ_LaPamPa',
-         description='for the execution of an sql query to the trainingn\
-          database, to obtain information about Universidad tres de febrero',
+# Dag definition for the ETL process
+with DAG('ETL_Univ_nacional_tres_de_febrero',
          start_date=datetime(2020, 3, 24),
          max_active_runs=3,
          schedule_interval='@hourly',
          default_args=default_args,
-         template_searchpath=f'{path_p}/include',
+         template_searchpath=f'{path_p}/airflow/include',
          catchup=False,
          ) as dag:
 
-# PythonOperator for extraaction function, commented above
-    extraction_task = PythonOperator(
-        task_id="Extraction",
-        python_callable=extraction,
-        op_kwargs={'database_id': 'training_db',
-                   'table_id': TABLE_ID}
 
+# PythonOperator for ETL function, commented above
+    ET_task = PythonOperator(
+        task_id="ET",
+        python_callable=extraction_transform_data, 
+        op_kwargs={'database_id': PG_ID, 'table_id': TABLE_ID}
     )
+
+
+
 # PythonOperator for logger function, commented above
     logging_task = PythonOperator(
         task_id="logguers",
-        python_callable=logger,
-        op_args= {f'{path_p}/logs/{TABLE_ID}'}
+        python_callable=logger
     )
 
-    logging_task 
-    extraction_task
-
+    logging_task
+    ET_task 
